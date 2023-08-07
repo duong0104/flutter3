1b1a2295b3e8e4d3951137dc78a2d9dd6e73cdd4
Dương Mai <duongmai23042001@gmail.com>
2023-08-07 13:16:13 +0700

    Giua ki

---
+++ GK2/tatwar_test-master/lib/presentation/widgets/app_elevated_button.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: unnecessary_import
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tatware_test/utilities/app_sizes.dart';
+
+class AppElevatedButton extends StatelessWidget {
+  const AppElevatedButton(
+      {Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: AppSizes.elevatedButtonHeight,
+      child: ElevatedButton(onPressed: onPressed, child: Text(text)),
+    );
+  }
+}
